55365daa0a0a4cc57d9853869fc692106019c606
unknown <amit.dcosta12@gmail.com>
2026-04-26 03:48:01 +0530

    Fix CORS for correct Vercel deployment again

--- frontend/src/services/activityService.js
+++ frontend/src/services/activityService.js
@@ -2,7 +2,7 @@ const BASE_URL = "https://activity-feed-system.onrender.com";
 
 export const fetchActivities = async (cursor) => {
   //let url = "http://localhost:5000/activities";
-  let url = `${BASE_URL}/activities`;
+  let url = `${BASE_URL}/api/activities`;
 
 
   if (cursor) {
@@ -19,7 +19,7 @@ export const fetchActivities = async (cursor) => {
 };
 
 export const createActivity = async (data) => {
-  const res = await fetch(`${BASE_URL}/activities`, {
+  const res = await fetch(`${BASE_URL}/api/activities`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
